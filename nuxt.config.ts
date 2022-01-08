e534251d060a7a47f7cd7a2cbdf59fddddc3a9ea
Marko Banušić <mbanusic@gmail.com>
2022-01-08 16:43:19 +0100

    install tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,21 +6,14 @@ export default defineNuxtConfig({
   },
   buildModules: [
     '@vueuse/nuxt',
-    '@unocss/nuxt',
     '@pinia/nuxt',
   ],
   vueuse: {
     ssrHandlers: true,
   },
-  unocss: {
-    uno: true,
-    attributify: true,
-    preflight: true,
-    icons: {
-      scale: 1.2,
+  build: {
+    postcss: {
+      postcssOptions: require('./postcss.config.js'),
     },
-    shortcuts: [
-      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ],
   },
 })
